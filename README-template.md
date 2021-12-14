c485032ed67dd9348bab36fb60a190e64bfe7da0
faozziyyah <omowunmidaud1@gmail.com>
2021-12-14 14:20:28 +0100

    completed

--- README-template.md
+++ README-template.md
@@ -1,6 +1,6 @@
 # Frontend Mentor - Calculator app solution
 
-This is a solution to the [Calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/calculator-app-9lteq5N29). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+This is a solution to the [Calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/calculator-app-9lteq5N29). with dark mode feature.
 
 ## Table of contents
 
@@ -16,8 +16,6 @@ This is a solution to the [Calculator app challenge on Frontend Mentor](https://
 - [Author](#author)
 - [Acknowledgments](#acknowledgments)
 
-**Note: Delete this note and update the table of contents based on what sections you keep.**
-
 ## Overview
 
 ### The challenge
@@ -27,23 +25,15 @@ Users should be able to:
 - See the size of the elements adjust based on their device's screen size
 - Perform mathmatical operations like addition, subtraction, multiplication, and division
 - Adjust the color theme based on their preference
-- **Bonus**: Have their initial theme preference checked using `prefers-color-scheme` and have any additional changes saved in the browser
 
 ### Screenshot
 
-![](./screenshot.jpg)
-
-Add a screenshot of your solution. The easiest way to do this is to use Firefox to view your project, right-click the page and select "Take a Screenshot". You can choose either a full-height screenshot or a cropped one based on how long the page is. If it's very long, it might be best to crop it.
-
-Alternatively, you can use a tool like [FireShot](https://getfireshot.com/) to take the screenshot. FireShot has a free option, so you don't need to purchase it. 
-
-Then crop/optimize/edit your image however you like, add it to your project, and update the file path in the image above.
-
-**Note: Delete this note and the paragraphs above when you add your screenshot. If you prefer not to add a screenshot, feel free to remove this entire section.**
+![](images.screenshot.png)
+![](images.screenshot1.png)
 
 ### Links
 
-- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Solution URL: [Add solution URL here](https://github.com/faozziyyah/js-calculator)
 - Live Site URL: [Add live site URL here](https://your-live-site-url.com)
 
 ## My process
@@ -55,59 +45,50 @@ Then crop/optimize/edit your image however you like, add it to your project, and
 - Flexbox
 - CSS Grid
 - Mobile-first workflow
-- [React](https://reactjs.org/) - JS library
-- [Next.js](https://nextjs.org/) - React framework
-- [Styled Components](https://styled-components.com/) - For styles
-
-**Note: These are just examples. Delete this note and replace the list above with your own choices**
 
 ### What I learned
-
-Use this section to recap over some of your major learnings while working through this project. Writing these out and providing code samples of areas you want to highlight is a great way to reinforce your own knowledge.
-
-To see how you can add code snippets, see below:
-
 ```html
-<h1>Some HTML code I'm proud of</h1>
+<h1>HTML layout of a calculator</h1>
+ <button type="button" class="operator" value="-">-</button>
+
+	  <button type="button" class="operator" value="*">&times;</button>
+
+	  <button type="button" class="operator" value="/">&divide;</button>
+	  
+	  <button type="button" value="7">7</button>
+	  <button type="button" value="8">8</button>
+	  <button type="button" value="9">9</button>
 ```
 ```css
-.proud-of-this-css {
-  color: papayawhip;
+.calculator-keys {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-gap: 20px;
+  padding: 20px;
 }
 ```
 ```js
-const proudOfThisFunc = () => {
+const calculator = {
+  displayValue: '0',
+  firstOperand: null,
+  waitingForSecondOperand: false,
+  operator: null,
+};
+
   console.log('🎉')
-}
 ```
 
-If you want more help with writing markdown, we'd recommend checking out [The Markdown Guide](https://www.markdownguide.org/) to learn more.
-
-**Note: Delete this note and the content within this section and replace with your own learnings.**
-
 ### Continued development
-
-Use this section to outline areas that you want to continue focusing on in future projects. These could be concepts you're still not completely comfortable with or techniques you found useful that you want to refine and perfect.
-
-**Note: Delete this note and the content within this section and replace with your own plans for continued development.**
+- sin, cos, tan functionality
 
 ### Useful resources
 
-- [Example resource 1](https://www.example.com) - This helped me for XYZ reason. I really liked this pattern and will use it going forward.
-- [Example resource 2](https://www.example.com) - This is an amazing article which helped me finally understand XYZ. I'd recommend it to anyone still learning this concept.
-
-**Note: Delete this note and replace the list above with resources that helped you during the challenge. These could come in handy for anyone viewing your solution or for yourself when you look back on this project in the future.**
+- [CSS grid](https://freshman.tech/css-grid-calculator/) - This helped me with CSS grid. I really liked this pattern and it's explanatory.
+- [Js calculator resources](https://freshman.tech/calculator/) - This is an amazing article which helped me finally understand javascript calculator. I'd recommend it to anyone still learning this concept.
 
 ## Author
 
-- Website - [Add your name here](https://www.your-site.com)
-- Frontend Mentor - [@yourusername](https://www.frontendmentor.io/profile/yourusername)
-- Twitter - [@yourusername](https://www.twitter.com/yourusername)
+- Website - [Faozziyyah](https://resume-cv-xi.vercel.app/)
+- Frontend Mentor - [@faozziyyah](https://www.frontendmentor.io/profile/faozziyyah)
+- Twitter - [@muslimahdev](https://www.twitter.com/muslimahdev?s=08)
 
-**Note: Delete this note and add/remove/edit lines above based on what links you'd like to share.**
-
-## Acknowledgments
-
-This is where you can give a hat tip to anyone who helped you out on this project. Perhaps you worked in a team or got some inspiration from someone else's solution. This is the perfect place to give them some credit.
-
-**Note: Delete this note and edit this section's content as necessary. If you completed this challenge by yourself, feel free to delete this section entirely.**
--- index.js
+++ index.js
@@ -1,160 +1,159 @@
-
 // calculator Object
 const calculator = {
-    displayValue: '0',
-    firstOperand: null,
-    waitingForSecondOperand: false,
-    operator: null,
-  };
+  displayValue: '0',
+  firstOperand: null,
+  waitingForSecondOperand: false,
+  operator: null,
+};
 
-  //input digits
+//input digits
 function inputDigit(digit){
-    const {displayValue, waitingForSecondOperand} = calculator;
-    
-    if (waitingForSecondOperand === true) {
-      calculator.displayValue = digit;
-      calculator.waitingForSecondOperand = false;
-    } else {
-    
-    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
-    }
-    
-  console.log(calculator);
+  const {displayValue, waitingForSecondOperand} = calculator;
   
+  if (waitingForSecondOperand === true) {
+    calculator.displayValue = digit;
+    calculator.waitingForSecondOperand = false;
+  } else {
+  
+  calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
   }
-
-  //update the display
-function updateDisplay(){
-    //select the element with class calculator-screen
-    const display = document.querySelector('.calculator-screen');
-    //update the value of the element with the content of displayValue
-    display.value = calculator.displayValue;
-  }
-  updateDisplay();
-
-  //handle key presses
-const keys = document.querySelector('.calculator-keys');
-keys.addEventListener('click', (event) => {
   
-  const {target} = event;
-  const {value} = target;
-  
- if (!target.matches('button')) {
-   return;
- }
- 
- switch (value) {
-   case '+':
-   case '-':
-   case '*':
-   case '/':
-   case '=':
-     // code
-     handleOperator(value);
-     break;
-     
-      case '.':
-     // code
-     inputDecimal(value);
-     break;
-     
-      case 'all-clear':
-     // code
-     resetCalculator();
-     break;
-   
-   default:
-     // code
-if (Number.isInteger(parseFloat(value))) {
-  inputDigit(target.value);
+console.log(calculator);
+
 }
 
- }
-  updateDisplay();
-  
+//update the display
+function updateDisplay(){
+  //select the element with class calculator-screen
+  const display = document.querySelector('.calculator-screen');
+  //update the value of the element with the content of displayValue
+  display.value = calculator.displayValue;
+}
+updateDisplay();
+
+//handle key presses
+const keys = document.querySelector('.calculator-keys');
+keys.addEventListener('click', (event) => {
+
+const {target} = event;
+const {value} = target;
+
+if (!target.matches('button')) {
+ return;
+}
+
+switch (value) {
+ case '+':
+ case '-':
+ case '*':
+ case '/':
+ case '=':
+   // code
+   handleOperator(value);
+   break;
+   
+    case '.':
+   // code
+   inputDecimal(value);
+   break;
+   
+    case 'all-clear':
+   // code
+   resetCalculator();
+   break;
+ 
+ default:
+   // code
+if (Number.isInteger(parseFloat(value))) {
+inputDigit(target.value);
+}
+
+}
+updateDisplay();
+
 });
 
 //input decimal point
 function inputDecimal(dot){
-  
-    if (calculator.waitingForSecondOperand === true) {
-      calculator.displayValue = '0.';
-      calculator.waitingForSecondOperand = false;
-      
-      return;
-    }
-    
-    if (!calculator.displayValue.includes(dot)) {
-      calculator.displayValue += dot;
-    }
-    
-  }
 
-  //handling operators
-function handleOperator(nextOperator){
+  if (calculator.waitingForSecondOperand === true) {
+    calculator.displayValue = '0.';
+    calculator.waitingForSecondOperand = false;
+    
+    return;
+  }
   
-    const {firstOperand, displayValue, operator} = calculator;
-    
-    const inputValue = parseFloat(displayValue);
-    
-    if (operator && calculator.waitingForSecondOperand) {
-      calculator.operator = nextOperator;
-      console.log(calculator);
-      return;
-    }
-    
-    if (firstOperand === null && !isNaN(inputValue)) {
-      calculator.firstOperand = inputValue;
-    } else if (operator) {
-      const result = calculate(firstOperand, inputValue, operator);
-      calculator.displayValue = `${parseFloat(result.toFixed(7))}`;
-      calculator.firstOperand = result;
-    }
-    
-    calculator.waitingForSecondOperand = true;
+  if (!calculator.displayValue.includes(dot)) {
+    calculator.displayValue += dot;
+  }
+  
+}
+
+//handling operators
+function handleOperator(nextOperator){
+
+  const {firstOperand, displayValue, operator} = calculator;
+  
+  const inputValue = parseFloat(displayValue);
+  
+  if (operator && calculator.waitingForSecondOperand) {
     calculator.operator = nextOperator;
     console.log(calculator);
-    
-  }
-
-  function calculate(firstOperand, secondOperand, operator) {
-  
-    if (operator === '+') {
-      return firstOperand + secondOperand;
-    } else if (operator === '-') {
-      return firstOperand - secondOperand;
-    } else if (operator === '*') {
-      return firstOperand * secondOperand;
-    } else if (operator === '/') {
-      return firstOperand / secondOperand;
-    }
-    
-    return secondOperand;
-    
+    return;
   }
   
-  //reset calculator
-  function resetCalculator(){
-    calculator.displayValue = '0';
-    calculator.firstOperand = 'null';
-    calculator.waitingForSecondOperand = 'false';
-    calculator.operator = 'null';
-    console.log(calculator);
-  }
-
-  //delete
-  let outputScreen = document.getElementById("calcscreen");
-  let deleteScreen = document.getElementById("del");
-  deleteScreen.addEventListener("click", deleteOutput);
-  function deleteOutput() {
-
-    
-    outputScreen.value = outputScreen.value.slice(0,-1);
-
+  if (firstOperand === null && !isNaN(inputValue)) {
+    calculator.firstOperand = inputValue;
+  } else if (operator) {
+    const result = calculate(firstOperand, inputValue, operator);
+    calculator.displayValue = `${parseFloat(result.toFixed(7))}`;
+    calculator.firstOperand = result;
   }
   
+  calculator.waitingForSecondOperand = true;
+  calculator.operator = nextOperator;
+  console.log(calculator);
+  
+}
 
-  /*   toggle calculator to theme      */
+function calculate(firstOperand, secondOperand, operator) {
+
+  if (operator === '+') {
+    return firstOperand + secondOperand;
+  } else if (operator === '-') {
+    return firstOperand - secondOperand;
+  } else if (operator === '*') {
+    return firstOperand * secondOperand;
+  } else if (operator === '/') {
+    return firstOperand / secondOperand;
+  }
+  
+  return secondOperand;
+  
+}
+
+//reset calculator
+function resetCalculator(){
+  calculator.displayValue = '0';
+  calculator.firstOperand = 'null';
+  calculator.waitingForSecondOperand = 'false';
+  calculator.operator = 'null';
+  console.log(calculator);
+}
+
+//delete
+let outputScreen = document.getElementById("calcscreen");
+let deleteScreen = document.getElementById("del");
+deleteScreen.addEventListener("click", deleteOutput);
+function deleteOutput() {
+
+  
+  outputScreen.value = outputScreen.value.slice(0,-1);
+
+}
+
+
+/*   toggle calculator to theme      */
 
 let theme2 = document.getElementById("theme2");
 let theme1 = document.getElementById("theme1");
@@ -172,44 +171,44 @@ let theme = document.getElementById("theme");
 let first = document.getElementById("first");
 let second = document.getElementById("second");
 let third = document.getElementById("third");
-  
+
 // theme 2
 
 theme2.addEventListener("click", showtheme2);
 
 function showtheme2() {
 
-  //let displayval = calcscreen.textContent;
+//let displayval = calcscreen.textContent;
 
-  bodybg.style.backgroundColor = "hsl(0, 0%, 90%)";
-  equalbtn.style.backgroundColor = "hsl(25, 98%, 40%)";
-  equalbtn.style.color = "white";
-  clearbtn.style.backgroundColor = "hsl(185, 42%, 37%)";
-  clearbtn.style.boxShadow = "none";
-  delbtn.style.backgroundColor = "hsl(185, 42%, 37%)";
-  delbtn.style.boxShadow = "none";
-  theme2.style.backgroundColor = "hsl(25, 98%, 40%)";
-  togglebg.style.backgroundColor = "hsl(0, 5%, 81%)";
-  calckeys.style.backgroundColor = "hsl(0, 5%, 81%)";
-  calcscreen.style.backgroundColor = "hsl(0, 0%, 93%)";
-  calcscreen.style.color = "black";
-  calc.style.color = "black";
-  theme.style.color = "black";
-  first.style.color = "black";
-  second.style.color = "black";
-  third.style.color = "black";
-  theme3.style.backgroundColor = "hsl(0, 5%, 81%)";
-  theme1.style.backgroundColor = "hsl(0, 5%, 81%)";
-  
-  let buttons = document.querySelectorAll(".btn");
- 
-  for (let i = 0; i < buttons.length; i++) {
-    if ( theme2.style.backgroundColor = "hsl(25, 98%, 40%)" ) {
-      buttons[i].style.backgroundColor = "hsl(45, 7%, 89%)";
-      buttons[i].style.boxShadow = "2px 2px 9px -2px hsl(35, 11%, 61%)";
-      buttons[i].style.color = "hsl(60, 10%, 19%)";
-    }  
-  }
+bodybg.style.backgroundColor = "hsl(0, 0%, 90%)";
+equalbtn.style.backgroundColor = "hsl(25, 98%, 40%)";
+equalbtn.style.color = "white";
+clearbtn.style.backgroundColor = "hsl(185, 42%, 37%)";
+clearbtn.style.boxShadow = "none";
+delbtn.style.backgroundColor = "hsl(185, 42%, 37%)";
+delbtn.style.boxShadow = "none";
+theme2.style.backgroundColor = "hsl(25, 98%, 40%)";
+togglebg.style.backgroundColor = "hsl(0, 5%, 81%)";
+calckeys.style.backgroundColor = "hsl(0, 5%, 81%)";
+calcscreen.style.backgroundColor = "hsl(0, 0%, 93%)";
+calcscreen.style.color = "black";
+calc.style.color = "black";
+theme.style.color = "black";
+first.style.color = "black";
+second.style.color = "black";
+third.style.color = "black";
+theme3.style.backgroundColor = "hsl(0, 5%, 81%)";
+theme1.style.backgroundColor = "hsl(0, 5%, 81%)";
+
+let buttons = document.querySelectorAll(".btn");
+
+for (let i = 0; i < buttons.length; i++) {
+  if ( theme2.style.backgroundColor = "hsl(25, 98%, 40%)" ) {
+    buttons[i].style.backgroundColor = "hsl(45, 7%, 89%)";
+    buttons[i].style.boxShadow = "2px 2px 9px -2px hsl(35, 11%, 61%)";
+    buttons[i].style.color = "hsl(60, 10%, 19%)";
+  }  
+}
 
 }
 
@@ -219,37 +218,37 @@ theme3.addEventListener("click", showtheme3);
 
 function showtheme3() {
 
-  //let displayval = calcscreen.textContent;
+//let displayval = calcscreen.textContent;
 
-  bodybg.style.backgroundColor = "hsl(268, 75%, 9%)";
-  equalbtn.style.backgroundColor = "hsl(176, 100%, 44%)";
-  equalbtn.style.color = "hsl(198, 20%, 13%)";
-  clearbtn.style.backgroundColor = "hsl(281, 89%, 26%)";
-  clearbtn.style.boxShadow = "2px 2px 9px -2px hsl(285, 91%, 52%)";
-  delbtn.style.backgroundColor = "hsl(281, 89%, 26%)";
-  delbtn.style.boxShadow = "2px 2px 9px -2px hsl(285, 91%, 52%)";
-  theme3.style.backgroundColor = "hsl(176, 100%, 44%)";
-  togglebg.style.backgroundColor = "hsl(268, 71%, 12%)";
-  calckeys.style.backgroundColor = "hsl(268, 71%, 12%";
-  calcscreen.style.backgroundColor = "hsl(268, 71%, 12%)";
-  calcscreen.style.color = "hsl(52, 100%, 62%)";
-  calc.style.color = "hsl(52, 100%, 62%)";
-  theme.style.color = "hsl(52, 100%, 62%)";
-  first.style.color = "hsl(52, 100%, 62%)";
-  second.style.color = "hsl(52, 100%, 62%)";
-  third.style.color = "hsl(52, 100%, 62%)";
-  theme2.style.backgroundColor = "hsl(268, 71%, 12%";
-  theme1.style.backgroundColor = "hsl(268, 71%, 12%";
-  //displayval.style.color = "black";
-  let buttons = document.querySelectorAll(".btn");
- 
-  for (let i = 0; i < buttons.length; i++) {
-    if ( theme3.style.backgroundColor = "hsl(176, 100%, 44%)" ) {
-      buttons[i].style.backgroundColor = "hsl(268, 47%, 21%)";
-      buttons[i].style.boxShadow = "2px 2px 9px -2px hsl(290, 70%, 36%)";
-      buttons[i].style.color = "hsl(52, 100%, 62%)";
-    }  
-  }
+bodybg.style.backgroundColor = "hsl(268, 75%, 9%)";
+equalbtn.style.backgroundColor = "hsl(176, 100%, 44%)";
+equalbtn.style.color = "hsl(198, 20%, 13%)";
+clearbtn.style.backgroundColor = "hsl(281, 89%, 26%)";
+clearbtn.style.boxShadow = "2px 2px 9px -2px hsl(285, 91%, 52%)";
+delbtn.style.backgroundColor = "hsl(281, 89%, 26%)";
+delbtn.style.boxShadow = "2px 2px 9px -2px hsl(285, 91%, 52%)";
+theme3.style.backgroundColor = "hsl(176, 100%, 44%)";
+togglebg.style.backgroundColor = "hsl(268, 71%, 12%)";
+calckeys.style.backgroundColor = "hsl(268, 71%, 12%";
+calcscreen.style.backgroundColor = "hsl(268, 71%, 12%)";
+calcscreen.style.color = "hsl(52, 100%, 62%)";
+calc.style.color = "hsl(52, 100%, 62%)";
+theme.style.color = "hsl(52, 100%, 62%)";
+first.style.color = "hsl(52, 100%, 62%)";
+second.style.color = "hsl(52, 100%, 62%)";
+third.style.color = "hsl(52, 100%, 62%)";
+theme2.style.backgroundColor = "hsl(268, 71%, 12%";
+theme1.style.backgroundColor = "hsl(268, 71%, 12%";
+//displayval.style.color = "black";
+let buttons = document.querySelectorAll(".btn");
+
+for (let i = 0; i < buttons.length; i++) {
+  if ( theme3.style.backgroundColor = "hsl(176, 100%, 44%)" ) {
+    buttons[i].style.backgroundColor = "hsl(268, 47%, 21%)";
+    buttons[i].style.boxShadow = "2px 2px 9px -2px hsl(290, 70%, 36%)";
+    buttons[i].style.color = "hsl(52, 100%, 62%)";
+  }  
+}
 
 }
 
@@ -260,44 +259,38 @@ theme1.addEventListener("click", showtheme1);
 
 function showtheme1() {
 
-  //let displayval = calcscreen.textContent;
+//let displayval = calcscreen.textContent;
 
-  bodybg.style.backgroundColor = "hsl(222, 26%, 31%)";
-  equalbtn.style.backgroundColor = "hsl(6, 63%, 50%)";
-  equalbtn.style.color = "white";
-  clearbtn.style.backgroundColor = "hsl(225, 21%, 49%)";
-  clearbtn.style.boxShadow = "none";
-  delbtn.style.backgroundColor = "hsl(221, 21%, 49%)";
-  delbtn.style.boxShadow = "none";
-  theme1.style.backgroundColor = "hsl(6, 63%, 50%)";
-  togglebg.style.backgroundColor = "hsl(223, 31%, 20%)";
-  calckeys.style.backgroundColor = "hsl(223, 31%, 20%";
-  calcscreen.style.backgroundColor = "hsl(224, 36%, 15%)";
-  calcscreen.style.color = "#fff";
-  calc.style.color = "#fff";
-  theme.style.color = "#fff";
-  first.style.color = "#fff";
-  second.style.color = "#fff";
-  third.style.color = "#fff";
-  theme2.style.backgroundColor = "hsl(223, 31%, 20%";
-  theme3.style.backgroundColor = "hsl(223, 31%, 20%";
+bodybg.style.backgroundColor = "hsl(222, 26%, 31%)";
+equalbtn.style.backgroundColor = "hsl(6, 63%, 50%)";
+equalbtn.style.color = "white";
+clearbtn.style.backgroundColor = "hsl(225, 21%, 49%)";
+clearbtn.style.boxShadow = "none";
+delbtn.style.backgroundColor = "hsl(221, 21%, 49%)";
+delbtn.style.boxShadow = "none";
+theme1.style.backgroundColor = "hsl(6, 63%, 50%)";
+togglebg.style.backgroundColor = "hsl(223, 31%, 20%)";
+calckeys.style.backgroundColor = "hsl(223, 31%, 20%";
+calcscreen.style.backgroundColor = "hsl(224, 36%, 15%)";
+calcscreen.style.color = "#fff";
+calc.style.color = "#fff";
+theme.style.color = "#fff";
+first.style.color = "#fff";
+second.style.color = "#fff";
+third.style.color = "#fff";
+theme2.style.backgroundColor = "hsl(223, 31%, 20%";
+theme3.style.backgroundColor = "hsl(223, 31%, 20%";
 
-  let buttons = document.querySelectorAll(".btn");
- 
-  for (let i = 0; i < buttons.length; i++) {
-    if ( theme1.style.backgroundColor = "hsl(6, 63%, 50%)" ) {
-      buttons[i].style.backgroundColor = "hsl(30, 25%, 89%)";
-      buttons[i].style.boxShadow = "2px 2px 9px -2px hsl(28, 16%, 65%)";
-      buttons[i].style.color = "hsl(221, 14%, 31%)";
-    }  
-  }
+let buttons = document.querySelectorAll(".btn");
+
+for (let i = 0; i < buttons.length; i++) {
+  if ( theme1.style.backgroundColor = "hsl(6, 63%, 50%)" ) {
+    buttons[i].style.backgroundColor = "hsl(30, 25%, 89%)";
+    buttons[i].style.boxShadow = "2px 2px 9px -2px hsl(28, 16%, 65%)";
+    buttons[i].style.color = "hsl(221, 14%, 31%)";
+  }  
+}
 
 
 
 }
-
-
-  
-
-  
-  
